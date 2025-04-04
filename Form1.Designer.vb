fa9588760fb9a2ef709b54c0dd860be1c73e2a77
falyArivelo <119029443+falyArivelo@users.noreply.github.com>
2025-04-04 16:03:23 +0300

    controls box

--- Form1.Designer.vb
+++ Form1.Designer.vb
@@ -24,8 +24,17 @@ Partial Class Form1
     Private Sub InitializeComponent()
         Dim CustomizableEdges1 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
         Dim CustomizableEdges2 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges3 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges4 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges5 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges6 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges7 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
+        Dim CustomizableEdges8 As Guna.UI2.WinForms.Suite.CustomizableEdges = New Guna.UI2.WinForms.Suite.CustomizableEdges()
         Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
         Guna2Button1 = New Guna.UI2.WinForms.Guna2Button()
+        Guna2ControlBox1 = New Guna.UI2.WinForms.Guna2ControlBox()
+        Guna2ControlBox2 = New Guna.UI2.WinForms.Guna2ControlBox()
+        Guna2ControlBox3 = New Guna.UI2.WinForms.Guna2ControlBox()
         SuspendLayout()
         ' 
         ' Guna2Button1
@@ -37,12 +46,56 @@ Partial Class Form1
         Guna2Button1.DisabledState.ForeColor = Color.FromArgb(CByte(141), CByte(141), CByte(141))
         Guna2Button1.Font = New Font("Segoe UI", 9.0F)
         Guna2Button1.ForeColor = Color.White
-        Guna2Button1.Location = New Point(395, 334)
+        Guna2Button1.Location = New Point(-1, 262)
         Guna2Button1.Name = "Guna2Button1"
         Guna2Button1.ShadowDecoration.CustomizableEdges = CustomizableEdges2
         Guna2Button1.Size = New Size(180, 45)
         Guna2Button1.TabIndex = 0
         Guna2Button1.Text = "Guna2Button1"
+        ' 
+        ' Guna2ControlBox1
+        ' 
+        Guna2ControlBox1.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Guna2ControlBox1.BackColor = Color.Transparent
+        Guna2ControlBox1.CustomizableEdges = CustomizableEdges3
+        Guna2ControlBox1.FillColor = Color.Transparent
+        Guna2ControlBox1.HoverState.FillColor = Color.Salmon
+        Guna2ControlBox1.HoverState.IconColor = Color.White
+        Guna2ControlBox1.IconColor = Color.DimGray
+        Guna2ControlBox1.Location = New Point(857, 0)
+        Guna2ControlBox1.Name = "Guna2ControlBox1"
+        Guna2ControlBox1.PressedColor = Color.Red
+        Guna2ControlBox1.ShadowDecoration.CustomizableEdges = CustomizableEdges4
+        Guna2ControlBox1.Size = New Size(45, 29)
+        Guna2ControlBox1.TabIndex = 1
+        ' 
+        ' Guna2ControlBox2
+        ' 
+        Guna2ControlBox2.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Guna2ControlBox2.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MaximizeBox
+        Guna2ControlBox2.CustomizableEdges = CustomizableEdges5
+        Guna2ControlBox2.FillColor = Color.Transparent
+        Guna2ControlBox2.IconColor = Color.DimGray
+        Guna2ControlBox2.Location = New Point(806, 0)
+        Guna2ControlBox2.Name = "Guna2ControlBox2"
+        Guna2ControlBox2.PressedColor = Color.Gainsboro
+        Guna2ControlBox2.ShadowDecoration.CustomizableEdges = CustomizableEdges6
+        Guna2ControlBox2.Size = New Size(45, 29)
+        Guna2ControlBox2.TabIndex = 1
+        ' 
+        ' Guna2ControlBox3
+        ' 
+        Guna2ControlBox3.Anchor = AnchorStyles.Top Or AnchorStyles.Right
+        Guna2ControlBox3.ControlBoxType = Guna.UI2.WinForms.Enums.ControlBoxType.MinimizeBox
+        Guna2ControlBox3.CustomizableEdges = CustomizableEdges7
+        Guna2ControlBox3.FillColor = Color.Transparent
+        Guna2ControlBox3.IconColor = Color.DimGray
+        Guna2ControlBox3.Location = New Point(755, 0)
+        Guna2ControlBox3.Name = "Guna2ControlBox3"
+        Guna2ControlBox3.PressedColor = Color.Gainsboro
+        Guna2ControlBox3.ShadowDecoration.CustomizableEdges = CustomizableEdges8
+        Guna2ControlBox3.Size = New Size(45, 29)
+        Guna2ControlBox3.TabIndex = 1
         ' 
         ' Form1
         ' 
@@ -50,6 +103,9 @@ Partial Class Form1
         AutoScaleMode = AutoScaleMode.Font
         BackColor = Color.WhiteSmoke
         ClientSize = New Size(900, 650)
+        Controls.Add(Guna2ControlBox3)
+        Controls.Add(Guna2ControlBox2)
+        Controls.Add(Guna2ControlBox1)
         Controls.Add(Guna2Button1)
         FormBorderStyle = FormBorderStyle.None
         Icon = CType(resources.GetObject("$this.Icon"), Icon)
@@ -60,6 +116,9 @@ Partial Class Form1
     End Sub
 
     Friend WithEvents Guna2Button1 As Guna.UI2.WinForms.Guna2Button
+    Friend WithEvents Guna2ControlBox1 As Guna.UI2.WinForms.Guna2ControlBox
+    Friend WithEvents Guna2ControlBox2 As Guna.UI2.WinForms.Guna2ControlBox
+    Friend WithEvents Guna2ControlBox3 As Guna.UI2.WinForms.Guna2ControlBox
 
 
 End Class
--- Form1.vb
+++ Form1.vb
@@ -2,4 +2,8 @@
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
     End Sub
+
+    Private Sub Guna2ControlBox1_Click(sender As Object, e As EventArgs) Handles Guna2ControlBox1.Click, Guna2ControlBox2.Click, Guna2ControlBox3.Click
+
+    End Sub
 End Class
